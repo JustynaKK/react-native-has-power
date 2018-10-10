a4bbc40eebc1afda0fe8de7754a694d4d9e4bb39
JustynaKK <justyna.kordiaczynska@gmail.com>
2018-10-10 15:12:45 +0200

    Updates

---
+++ precache-manifest.d076e62ae553c6febb858503e3532d22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9967be7e3b2a1d0ea817",
+    "url": "/react-native-has-power/static/js/runtime~main.9967be7e.js"
+  },
+  {
+    "revision": "294f2c09f6d87690758f",
+    "url": "/react-native-has-power/static/js/main.294f2c09.chunk.js"
+  },
+  {
+    "revision": "2d0e2afb9f84e8360784",
+    "url": "/react-native-has-power/static/js/1.2d0e2afb.chunk.js"
+  },
+  {
+    "revision": "294f2c09f6d87690758f",
+    "url": "/react-native-has-power/static/css/main.e583d298.chunk.css"
+  },
+  {
+    "revision": "9a0e246e9c9020bc957451a468f265bb",
+    "url": "/react-native-has-power/index.html"
+  }
+];
